d5a257d1cddbe90c427e429febaa5468239b9df7
Laboratorio2 <Laboratorio2@172.16.12.134>
2018-04-20 08:25:07 -0600

    primer commit al repositorio

---
+++ src/Ejemplos.java
@@ -0,0 +1,34 @@
+
+import javax.swing.JOptionPane;
+
+
+public class Ejemplos {
+    public static void main(String[] args)
+    {
+        /*int[] edad;
+        edad = new int[10];
+        
+        double[] precio = new double[3];
+        
+        for(int i=0; i<precio.length; i++)
+        {
+            double total = 0;
+            
+            precio[i] = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese el numero: "));
+            total += precio[i];
+        }
+        
+        JOptionPane.showMessageDialog(null, "Total: " + total);
+        */
+        
+        int seguir;
+        String nombre;
+        
+        do{
+            nombre = JOptionPane.showInputDialog("Ingrese su nombre");
+            JOptionPane.showMessageDialog(null, "Hola" + nombre);
+            seguir = JOptionPane.showConfirmDialog(null, "¿Desea cntinuar?", "Confirmar", JOptionPane.YES_NO_OPTION);
+        }
+        while(seguir==JOptionPane.YES_OPTION);
+    }
+}
---
+++ src/ejemplo.java
@@ -0,0 +1,10 @@
+
+public class ejemplo {
+    public static void main(String[] args)
+    {
+        int seguir;
+        String nombre;
+        
+        
+    }
+}
